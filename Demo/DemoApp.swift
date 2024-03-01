9856037b713e56992bc937c5f21a6d063efa539f
Billeh77 <billehemile@gmail.com>
2024-03-01 18:57:40 -0500

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Emile Billeh on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
